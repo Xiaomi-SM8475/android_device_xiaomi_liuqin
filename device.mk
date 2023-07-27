d720c0ac357c5ed085133c9604befbb97c6e0d49
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-07-27 17:20:32 +0200

    liuqin: Import fingerprint HAL
    Change-Id: I6fc0f7a65ba87394526f6512641506b5cce93a8d

--- device.mk
+++ device.mk
@@ -115,6 +115,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprint
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor

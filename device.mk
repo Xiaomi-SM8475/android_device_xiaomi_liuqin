38a692a435d21a0e0d8b80c5f5f84e4de90065ce
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-08-03 11:16:45 +0200

    liuqin: Build custom Xiaomi sensors multihal
    Change-Id: I5444796c54f13d9a9572f0085734f3f345cb0098

--- device.mk
+++ device.mk
@@ -204,7 +204,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.1-service.xiaomi-multihal \
     libsensorndkbridge
 
 # Setup Wizard

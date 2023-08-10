bdc5c8e70e483140f053e7edffb5a83bd2cfa7f8
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-08-10 23:45:12 +0200

    liuqin: Allow Setup Wizard rotation
    Change-Id: Ifc769dbac874528d43830a4cfee40c95c4c48719

--- device.mk
+++ device.mk
@@ -206,6 +206,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge
 
+# Setup Wizard
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.setupwizard.rotation_locked=false
+
 # Tablet
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml \

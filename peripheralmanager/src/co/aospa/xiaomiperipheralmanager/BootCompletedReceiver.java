ef1163324feb49d509413338de1cb23128fbfdd3
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-08-10 12:08:46 +0200

    liuqin: Add Xiaomi Pen support to Xiaomi Peripheral Manager
    Change-Id: I2698b754dd062d581f059456670639c71658f3fc

--- peripheralmanager/src/co/aospa/xiaomiperipheralmanager/BootCompletedReceiver.java
+++ peripheralmanager/src/co/aospa/xiaomiperipheralmanager/BootCompletedReceiver.java
@@ -20,5 +20,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         KeyboardUtils.setup(context);
+        PenUtils.setup(context);
     }
 }

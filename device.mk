8bf73393c9f3fd0542a783a5023a2b3c5709ad0d
Tobias Merkel <stylogey@gmail.com>
2023-10-13 02:18:11 +0200

    liuqin: Build android.hardware.drm-service.clearkey
    Change-Id: I026ac2d84156f0c083adb118bc51c4fd18e5b450

--- device.mk
+++ device.mk
@@ -119,7 +119,9 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.4.vendor \
+    android.hardware.drm-service.clearkey \
+    libdrm.vendor
 
 # Fastbootd
 PRODUCT_PACKAGES += \

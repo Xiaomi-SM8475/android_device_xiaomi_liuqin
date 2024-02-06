00511860ca74bdf566c93a762949277761f7a122
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-02-06 16:03:07 +0100

    liuqin: Disable HW overlays on pen usage
     * looks like a hack but apparently Xiaomi does the same thing
    
    Change-Id: I0558d31a131dd148afd30119c66311595c79ab8f

--- parts/src/com/xiaomi/settings/peripheral/PenUtilsService.java
+++ parts/src/com/xiaomi/settings/peripheral/PenUtilsService.java
@@ -14,6 +14,9 @@ import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.hardware.input.InputManager;
 import android.hardware.input.InputManager.InputDeviceListener;
 import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.util.Log;
 import android.view.InputDevice;
 
@@ -28,11 +31,16 @@ public class PenUtilsService extends Service {
 
     private static final String STYLUS_KEY = "stylus_switch_key";
 
+    private static final String SURFACE_FLINGER_SERVICE_KEY = "SurfaceFlinger";
+    private static final String SURFACE_COMPOSER_INTERFACE_KEY = "android.ui.ISurfaceComposer";
+    private static final int SURFACE_FLINGER_DISABLE_OVERLAYS_CODE = 1008;
+
     private static boolean mIsPenModeEnabled;
     private static boolean mIsPenModeForced;
 
     private static InputManager mInputManager;
     private static SharedPreferences mSharedPrefs;
+    private static IBinder mSurfaceFlinger;
 
     @Override
     public void onCreate() {
@@ -43,6 +51,7 @@ public class PenUtilsService extends Service {
         mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         mSharedPrefs.registerOnSharedPreferenceChangeListener(mSharedPrefsListener);
         mIsPenModeForced = mSharedPrefs.getBoolean(STYLUS_KEY, false);
+        mSurfaceFlinger = ServiceManager.getService(SURFACE_FLINGER_SERVICE_KEY);
     }
 
     @Override
@@ -68,12 +77,24 @@ public class PenUtilsService extends Service {
         TfWrapper.setTouchFeature(new TfWrapper.TfParams(20, mIsPenModeEnabled ? 18 : 2));
     }
 
+    private void updateHardwareOverlaysSetting() {
+        if (mSurfaceFlinger == null) return;
+        try {
+            final Parcel data = Parcel.obtain();
+            data.writeInterfaceToken(SURFACE_COMPOSER_INTERFACE_KEY);
+            data.writeInt(mIsPenModeEnabled ? 1 : 0);
+            mSurfaceFlinger.transact(SURFACE_FLINGER_DISABLE_OVERLAYS_CODE, data, null, 0);
+            data.recycle();
+        } catch (RemoteException ex) { }
+    }
+
     private void refreshPenMode() {
         if (mIsPenModeForced) {
             if (DEBUG) Log.d(TAG, "refreshPenMode: Pen Mode forced");
             if (!mIsPenModeEnabled) {
                 mIsPenModeEnabled = true;
                 updatePenMode();
+                updateHardwareOverlaysSetting();
             }
             return;
         }
@@ -83,6 +104,7 @@ public class PenUtilsService extends Service {
                 if (!mIsPenModeEnabled) {
                     mIsPenModeEnabled = true;
                     updatePenMode();
+                    updateHardwareOverlaysSetting();
                 }
                 return;
             }
@@ -91,6 +113,7 @@ public class PenUtilsService extends Service {
         if (mIsPenModeEnabled) {
             mIsPenModeEnabled = false;
             updatePenMode();
+            updateHardwareOverlaysSetting();
         }
     }
 

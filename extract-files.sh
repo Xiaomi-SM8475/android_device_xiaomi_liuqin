627ca6e2f7bbb9aedcc532180d89d12b64114917
Arian <arian.kulmer@web.de>
2024-02-28 16:53:29 +0100

    liuqin: Patch citsensorservice to work with OSS libdisplayconfig.qti
    For some reason xiaomi has duplicated the Init method as ClientImplGet.
    (or it gets automatically generated for them due to some other differences?)
    However, our OSS libdisplayconfig.qti doesn't provide this symbol, but the
    Init method takes the same parameters and seems to be sufficient for
    citsensorservice to work.
    
    Change-Id: I26a88eb379b517f147932310de1da51d65a31c6b

--- extract-files.sh
+++ extract-files.sh
@@ -73,6 +73,9 @@ function blob_fixup() {
         vendor/lib64/hw/audio.primary.taro.so)
             "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;
+        vendor/lib64/hw/vendor.xiaomi.sensor.citsensorservice@2.0-impl.so)
+            sed -i 's/_ZN13DisplayConfig10ClientImpl13ClientImplGetENSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPNS_14ConfigCallbackE/_ZN13DisplayConfig10ClientImpl4InitENSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPNS_14ConfigCallbackE\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0/g' "${2}"
+            ;;
     esac
 }
 

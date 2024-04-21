63fbc7e0af18010be0c575bd949f11038bbadab1
LuK1337 <priv.luk@gmail.com>
2024-04-21 10:01:48 +0200

    liuqin: Enable settings large screen optimization for tablets
    This was set to true by default prior to U QPR2.
    
    Test: Open settings and notice that dual pane layout is back
    
    Change-Id: Ifed56f4c63539d56672d8d915ea12d0654e64880

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,6 +119,7 @@ TARGET_POWER_FEATURE_EXT_LIB := //$(DEVICE_PATH):libpowerfeature_ext_liuqin
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/properties/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/properties/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
 
 # Recovery

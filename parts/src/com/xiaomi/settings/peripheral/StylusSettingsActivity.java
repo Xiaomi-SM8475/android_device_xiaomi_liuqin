d02be0cba3ba686317e887ae57471e26f557c8f4
Lee HoJoon <leehojoonlee0820@gmail.com>
2024-12-01 05:48:12 +0900

    liuqin: parts: Replace widget.R with collapsingtoolbar.R
    Co-Authored-By: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Co-Authored-By: markakash <akashniki@gmail.com>

--- parts/src/com/xiaomi/settings/peripheral/StylusSettingsActivity.java
+++ parts/src/com/xiaomi/settings/peripheral/StylusSettingsActivity.java
@@ -19,7 +19,7 @@ package com.xiaomi.settings.peripheral;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
+import com.android.settingslib.collapsingtoolbar.R;
 
 public class StylusSettingsActivity extends CollapsingToolbarBaseActivity {
 

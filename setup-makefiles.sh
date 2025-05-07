428c98695b82f8697ad7d4bfdbb317c92cdc5019
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-05-07 23:04:28 +0900

    liuqin: Update to OS2.0.8.0.VMYCNXM
    Change-Id: I678e05d0b1ca40dbb40014f2728d4f45f2a7b566

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -28,7 +28,7 @@ function vendor_imports() {
 		"vendor/qcom/common/system/telephony",
 		"vendor/qcom/common/vendor/adreno-s",
 		"vendor/qcom/common/vendor/display/5.10",
-        "vendor/qcom/common/vendor/media",
+		"vendor/qcom/common/vendor/media",
 EOF
 }
 
